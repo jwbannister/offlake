f49cb268fdd9fa77159595aabd80fc889bbf3036
John Bannister <jbannister@airsci.com>
2016-07-11 21:36:45 -0700

    built list of violation dates with offshore windroses

--- R/teom_functions.R
+++ R/teom_functions.R
@@ -16,75 +16,20 @@
 pull_mfile_data<- function(date1, date2){
   print("pulling wind and pm10 data from archive.mfile_data...")
   mfile_df <- 
-    query_owenslake(paste0("SELECT did, datetime, deployment_id, dir, aspd, 
-                           teom, qaqc_level_id 
-                           FROM archive.mfile_data 
+    query_owenslake(paste0("SELECT i.deployment, d.datetime, d.dir, d.aspd, 
+                           d.teom, d.qaqc_level_id, i.easting_utm, 
+                           i.northing_utm
+                           FROM archive.mfile_data d
+                           JOIN instruments.deployments i
+                           ON d.deployment_id=i.deployment_id
                            WHERE datetime > timestamp '", date1, 
                            "' AND datetime < timestamp '", date2, "';"))
   if (sum(!is.na(mfile_df$qaqc_level_id))>0) print("QA/QC failures in data!")
   mfile_df <- mfile_df[is.na(mfile_df$qaqc_level_id), ]
-  mfile_df <- select(mfile_df, data.id = did, datetime, 
-                     deployment.id = deployment_id, 
-                     wd = dir, ws = aspd, pm10.avg=teom)
+  mfile_df <- select(mfile_df, deployment, datetime,  wd = dir, ws = aspd, 
+                     pm10.avg=teom, x=easting_utm, y=northing_utm)
   # remove duplicated data lines (problem in database)
   mfile_df <- mfile_df[!duplicated(mfile_df[ , -1]), ]
   mfile_df
 }
 
-#' Pull instrument location data.
-#' 
-#' pull instrument UTM coordinates and deployment description from 
-#' instruments.deployments in AirSci PostgreSQL database, base on deployment_id 
-#' number. 
-#' 
-#' @param deploys Numerical. Vector of deployment ids.
-#' @return Data frame.
-#' @examples
-#' pull_locations(c(1719, 1718, 1849))
-pull_locations <- function(deploys){
-  deploys <- paste0("(", paste(deploys, collapse=", "), ")")
-  station_locs <- 
-    query_owenslake(paste0("SELECT deployment_id, deployment, northing_utm, 
-                           easting_utm, description 
-                           FROM instruments.deployments 
-                           WHERE deployment_id IN ", deploys))
-  colnames(station_locs) <- gsub("_", ".", colnames(station_locs))
-  station_locs <- select(station_locs, -description)
-  station_locs <- rename(station_locs, x=easting.utm, y=northing.utm)
-  station_locs
-}
-
-#' Pull report-quality PM10 data
-#'
-#' Pull PM10 data for reporting from AirSci PostgreSQL database.
-#' 
-#' @param x A number.
-#' @param deploys Numerical. Vector of deployment ids.
-#' @return Data frame.
-#' @examples
-#' pull_pm10(c(1719, 1718, 1849))
-pull_pm10 <- function(date1, date2, deploys){
-  print("pulling PM10 data from teoms.deployment_data...")
-  deploys <- paste0("(", paste(deploys, collapse=", "), ")")
-  pm10_df <- 
-    query_owenslake(paste0("SELECT d.deployment, 
-                           file_uploads.date_trunc_hour(datetime) 
-                           AS datetime_hour, 
-                           AVG(dd.teomamc) 
-                           FROM teoms.deployment_data dd 
-                           JOIN instruments.deployments d 
-                           ON dd.deployment_id=d.deployment_id 
-                           WHERE datetime > timestamp '", date1,  
-                           "' AND datetime < timestamp '", date2, 
-                           "' AND dd.deployment_id IN ", deploys, 
-                           " GROUP BY d.deployment, 
-                           file_uploads.date_trunc_hour(datetime) 
-                           ORDER BY d.deployment, datetime_hour"))
-  pm10_df <- rename(pm10_df, pm10.avg=avg)
-  print(paste0("removing ", nrow(filter(pm10_df, pm10.avg < -35)),
-               " hours with pm10.avg < -35"))
-  pm10_df <- filter(pm10_df, pm10.avg > -35)
-  # remove duplicated data lines (problem in database)
-  pm10_df <- pm10_df[!duplicated(pm10_df[ , -1]), ]
-  pm10_df
-}
--- scripts/offlake_windroses.R
+++ scripts/offlake_windroses.R
@@ -1,10 +1,42 @@
 load_all()
 load_all("~/analysis/Rowens")
+load_all("~/analysis/windroseR")
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(rgdal)
 
 start_date <- mdy("01-01-2016")
 end_date <- start_date %m+% months(6) %m-% days(1)
 
 mfile_df <- pull_mfile_data(start_date, end_date)
+offshore_sites <- c("Lone Pine", "Keeler", 
+                    "North Beach", "Lizard Tail", "Mill Site", "Flat Rock",
+                    "Shell Cut", "Dirty Socks", "Olancha", "Stanley")
+mfile_df <- filter(mfile_df, deployment %in% offshore_sites)
+mfile_sum <- mfile_df %>% distinct(deployment, x, y)
+p1 <- ggplot() +
+  geom_path(data=shoreline_poly, mapping=aes(x=x, y=y)) +
+  geom_point(data=mfile_sum, mapping=aes(x=x, y=y)) +
+  geom_text(data=mfile_sum, mapping=aes(x=x, y=y, label=deployment)) +
+  coord_equal()
+  
+mfile_df$date <- as.Date(mfile_df$datetime)
+mfile_df <- mfile_df[complete.cases(mfile_df), ]
+violations <- mfile_df %>% group_by(date, deployment) %>%
+  summarize(daily.pm10=sum(pm10.avg, na.rm=T)/24) %>% 
+  filter(daily.pm10>150) 
+
+roses <- vector(mode="list", length=length(unique(violations$date)))
+names(roses) <- as.character(unique(violations$date))
+for (i in as.character(unique(violations$date))){
+  plots <- vector(mode="list", length=length(offshore_sites))
+  names(plots) <- offshore_sites
+  for (j in offshore_sites){
+    if (nrow(filter(mfile_df, date==i, deployment==j))==0) next 
+    plots[[j]] <- mfile_df %>% filter(date==i, deployment==j) %>%
+      plot_rose_image_only(., value='ws', dir='wd', 
+                           valueseq=c(5, 10, 15, 20))
+  }
+  roses[[i]] <- plots
+}
